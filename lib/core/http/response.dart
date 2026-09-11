c48a52104d36f38e74726f4650fc5db3b139c264
Adeloye Adetayo <adeloyeadetayo8@gmail.com>
2026-09-11 03:33:02 +0100

    feat(http): add response model

---
+++ lib/core/http/response.dart
@@ -0,0 +1,13 @@
+class HttpResponse {
+  final int statusCode;
+  final Map<String, String> headers;
+  final String body;
+  final Duration duration;
+
+  const HttpResponse({
+    required this.statusCode,
+    required this.headers,
+    required this.body,
+    required this.duration,
+  });
+}
