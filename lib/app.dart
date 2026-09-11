4032ef77f540bcb7c9d571fc240dba72db8bc226
Adeloye Adetayo <adeloyeadetayo8@gmail.com>
2026-09-11 03:35:38 +0100

    feat(app): add Lurc application shell

---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'screens/request/request_screen.dart';
+
+class LurcApp extends StatelessWidget {
+  const LurcApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lurc',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+        useMaterial3: true,
+      ),
+      home: const RequestScreen(),
+    );
+  }
+}
