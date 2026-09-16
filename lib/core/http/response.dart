ace33bb41b93de07e7a8d2960bf52fd472d3f75a
Adeloye Adetayo <adeloyeadetayo8@gmail.com>
2026-09-16 16:15:11 +0100

    style(lint): order response model members

--- lib/core/http/response.dart
+++ lib/core/http/response.dart
@@ -1,13 +1,13 @@
 class HttpResponse {
-  final int statusCode;
-  final Map<String, String> headers;
-  final String body;
-  final Duration duration;
-
   const HttpResponse({
     required this.statusCode,
     required this.headers,
     required this.body,
     required this.duration,
   });
+
+  final int statusCode;
+  final Map<String, String> headers;
+  final String body;
+  final Duration duration;
 }
