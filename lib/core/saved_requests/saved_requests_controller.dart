a0b359c3a8cbefa4e066aa93500443c3467925f2
Adeloye Adetayo <adeloyeadetayo8@gmail.com>
2026-09-17 09:15:43 +0100

    fix(saved-requests): import Riverpod family type from public API

--- lib/core/saved_requests/saved_requests_controller.dart
+++ lib/core/saved_requests/saved_requests_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/misc.dart' show ProviderFamily;
 import 'package:lurc/core/saved_requests/collection.dart';
 import 'package:lurc/core/saved_requests/saved_request.dart';
 import 'package:lurc/core/saved_requests/saved_requests_repository.dart';
