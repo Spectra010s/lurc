e0630ba9bd0f4c0d08ad43fdae47eb3711b5e232
Adeloye Adetayo <adeloyeadetayo8@gmail.com>
2026-09-19 17:32:05 +0100

    feat(settings): wire theme preference into app

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:lurc/screens/request/request_screen.dart';
 import 'package:lurc/theme/lurc_theme.dart';
+import 'package:lurc/theme/theme_mode_controller.dart';
 
-class LurcApp extends StatelessWidget {
+class LurcApp extends StatefulWidget {
   const new({super.key});
 
   @override
+  State<LurcApp> createState() => _LurcAppState();
+}
+
+class _LurcAppState extends State<LurcApp> {
+  ThemeModeController? _themeController;
+
+  @override
+  void initState() {
+    super.initState();
+    ThemeModeController.load().then((controller) {
+      if (!mounted) return;
+      setState(() => _themeController = controller);
+    });
+  }
+
+  @override
+  void dispose() {
+    _themeController?.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final controller = _themeController;
     return MaterialApp(
       title: 'Lurc',
       theme: LurcTheme.light(),
       darkTheme: LurcTheme.dark(),
-      themeMode: ThemeMode.system,
-      home: const RequestScreen(),
+      themeMode: controller?.mode ?? ThemeMode.system,
+      home: RequestScreen(themeController: controller),
     );
   }
 }
