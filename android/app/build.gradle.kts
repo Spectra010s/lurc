daccf119c7ddc94518c6373787f8f074cde25a3e
Adeloye Adetayo <adeloyeadetayo8@gmail.com>
2026-09-20 15:16:28 +0100

    build(android): configure release signing

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,8 +1,21 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
+
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
+fun signingValue(environmentName: String, propertyName: String): String? =
+    System.getenv(environmentName)
+        ?: keystoreProperties.getProperty(propertyName)
 
 android {
     namespace = "com.lurc.app"
@@ -15,25 +28,28 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.lurc.app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        // Uses the version code from pubspec.yaml. When using split APKs, 1000 * ABI_VERSION
-        // is added automatically by Flutter. (https://developer.android.com/studio/build/configure-apk-splits#configure-APK-versions)
-        // You can force using the value of versionCode by specifying the value of `-P force-version-code-ignoring-abi=true`
-        // flag during build.
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            val keystorePath = signingValue("LURC_KEYSTORE_PATH", "storeFile")
+            if (keystorePath != null) {
+                storeFile = file(keystorePath)
+            }
+            storePassword = signingValue("LURC_KEYSTORE_PASSWORD", "storePassword")
+            keyAlias = signingValue("LURC_KEY_ALIAS", "keyAlias")
+            keyPassword = signingValue("LURC_KEY_PASSWORD", "keyPassword")
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
